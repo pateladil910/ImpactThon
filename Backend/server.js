e861c72e57c8d3e8786fb1f411f874e422b25019
pateladil910 <adilp4534@gmail.com>
2026-05-07 12:04:09 +0530

    changes in server.js file

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const path = require("path");
 require("dotenv").config();
 
+const authRoutes = require("./routes/auth");
 const detectionRoutes = require("./routes/detection");
 const analyticsRoutes = require("./routes/analytics");
 const sendAlertEmail = require("./utils/sendEmail");
