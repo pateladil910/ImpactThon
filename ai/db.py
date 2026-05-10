cba947caa9f42dfc7bac4c7a5748f29fdfa235f5
pateladil910 <adilp4534@gmail.com>
2026-05-10 10:56:28 +0530

    changes

--- ai/db.py
+++ ai/db.py
@@ -1,6 +1,8 @@
+import os
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
+mongo_uri = os.environ.get("MONGO_URI", "mongodb://localhost:27017/")
+client = MongoClient(mongo_uri)
 db = client["ai_safety"]
 
 history_collection = db["history"]
