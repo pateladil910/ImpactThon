618c6c1fa087017bffaf7c7a7b9f2c8d96053b40
pateladil910 <adilp4534@gmail.com>
2026-05-10 11:15:39 +0530

    changes

--- ai/db.py
+++ ai/db.py
@@ -3,6 +3,6 @@ from pymongo import MongoClient
 
 mongo_uri = os.environ.get("MONGO_URI", "mongodb://localhost:27017/")
 client = MongoClient(mongo_uri)
-db = client["ai_safety"]
+db = client["test"]
 
 history_collection = db["history"]
